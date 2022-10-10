81cfdbe50a4b35b0427261835c14de542ffba2cd
prime0x2 <tigerhex15@gmail.com>
2022-10-10 23:56:29 +0600

    side elemets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import LeftSide from './components/SideElement/LeftSide';
+import RightSide from './components/SideElement/RightSide';
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<LeftSide />
+			<RightSide />
 			<main>
 				<Header />
 			</main>
---
+++ src/components/SideElement/RightSide.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RightSide = () => {
+	return (
+		<div className='right_side_element'>
+			<div className='inner'>
+				<a href='mailto:brittany.chiang@gmail.com'>
+					brittany.chiang@gmail.com
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default RightSide;
