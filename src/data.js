b29509c8825240f2832f3825fb0b3b53332c502d
prime0x2 <tigerhex15@gmail.com>
2022-10-10 20:21:43 +0600

    project setup

---
+++ src/data.js
@@ -0,0 +1,132 @@
+export const jobs = [
+    {
+        id: 1,
+        title: "Engineer",
+        company: "Upstatement",
+        time: "May 2018 - Present",
+        points: [
+            "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+            "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify",
+            "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis"
+        ]
+    },
+    {
+        id: 2,
+        title: "Studio Developer",
+        company: "Scout",
+        time: "January - April 2018",
+        points: [
+            "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
+            "Helped solidify a brand direction for blistabloc that spans both packaging and web",
+            "Interfaced with clients on a weekly basis, providing technological expertise"
+        ]
+    },
+    {
+        id: 3,
+        title: "UI Engineer Co-op",
+        company: "Apple",
+        time: "July - December 2017",
+        points: [
+            "Developed and shipped highly interactive web applications for Apple Music using Ember.js",
+            "Built and shipped the Apple Music Extension within Facebook Messenger leveraging third-party and internal APIs",
+            "Architected and implemented the front-end of Apple Music's embeddable web player widget, which lets users log in and listen to full songs in the browser",
+            "Contributed extensively to MusicKit.js, a JavaScript framework that allows developers to add an Apple Music player to their web apps"
+        ]
+    },
+    {
+        id: 4,
+        title: "Studio Developer",
+        company: "Scout",
+        time: "January - June 2017",
+        points: [
+            "Collaborated with a small team of student designers to spearhead a new brand and design system for Scout’s inaugural student-led design conference at Northeastern",
+            "Worked closely with designers and management team to develop, document, and manage the conference’s marketing website using Jekyll, Sass, and JavaScript",
+        ]
+    },
+    {
+        id: 5,
+        title: "Software Engineer Co-op",
+        company: "Starry",
+        time: "July - December 2016",
+        points: [
+            "Engineered and maintained major features of Starry's customer-facing web app using ES6, Handlebars, Backbone, Marionette and CSS",
+            "Proposed and implemented scalable solutions to issues identified with cloud services and applications responsible for communicating with Starry Station",
+            "Interfaced with user experience designers and other developers to ensure thoughtful and coherent user experiences across Starry’s iOS and Android mobile apps"
+        ]
+    },
+    {
+        id: 6,
+        title: "Creative Technologist Co-op",
+        company: "MullenLowe",
+        time: "July - December 2015",
+        points: [
+            "Developed and maintained code for in-house and client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery",
+            "Manually tested sites in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness",
+            "Clients included JetBlue, Lovesac, U.S. Cellular, U.S. Department of Defense, and more"
+        ]
+    }
+]
+
+
+export const projects = [
+    {
+        id: 1,
+        title: "Integrating Algolia Search with WordPress Multisite",
+        description: "Building a custom multisite compatible WordPress plugin to build global search with Algolia",
+        tech: [
+            "Algolia",
+            "WordPress",
+            "PHP",
+        ]
+    },
+    {
+        id: 2,
+        title: "Time to Have More Fun",
+        description: "A single page web app for helping me choose where to travel, built with Next.js, Firebase, and Tailwind CSS",
+        tech: [
+            "Next.js",
+            "Tailwind CSS",
+            "Firebase",
+        ]
+    },
+    {
+        id: 3,
+        title: "Building a Headless Mobile App CMS From Scratch",
+        description: "Find out how we built a custom headless CMS with Node, Express, and Firebase for a project at Upstatement",
+        tech: [
+            "Node",
+            "Express",
+            "Firebase",
+            "Vue"
+        ]
+    },
+    {
+        id: 4,
+        title: "OctoProfile",
+        description: "A nicer look at your GitHub profile and repo stats. Includes data visualizations of your top languages, starred repositories, and sort through your top repos by number of stars, forks, and size.",
+        tech: [
+            "Next.js",
+            "Chart.js",
+            "Github API"
+        ]
+    },
+    {
+        id: 5,
+        title: "Google Keep Clone",
+        description: "A simple Google Keep clone built with Vue and Firebase.",
+        tech: [
+            "Vue",
+            "Firebase",
+        ]
+    },
+    {
+        id: 6,
+        title: "Apple Music Embeddable Web Player Widget",
+        description: "Embeddable web player widget for Apple Music that lets users log in and listen to full song playback in the browser.",
+        tech: [
+            "MusicKit.js",
+            "JavaScript",
+            "SCSS"
+        ]
+    }
+]
